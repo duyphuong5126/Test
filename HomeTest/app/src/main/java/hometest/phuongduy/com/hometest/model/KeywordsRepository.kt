6ae1a28aa4c4856b4856f997f1fb6003cbc49ffa
Phuong Nguyen Hoang Duy <nhdphuong@fossil.com>
2018-10-06 18:23:31 +0700

    Update keyword repository

--- HomeTest/app/src/main/java/hometest/phuongduy/com/hometest/model/KeywordsRepository.kt
+++ HomeTest/app/src/main/java/hometest/phuongduy/com/hometest/model/KeywordsRepository.kt
@@ -1,6 +1,7 @@
 package hometest.phuongduy.com.hometest.model
 
 import android.arch.persistence.room.Room
+import android.support.annotation.WorkerThread
 import android.util.Log
 import hometest.phuongduy.com.hometest.Constants
 import hometest.phuongduy.com.hometest.HomeTestApplication
@@ -36,55 +37,66 @@ object KeywordsRepository {
             if (shouldFetch) {
                 Log.d(TAG, "Should fetch from remote")
                 fetchKeywordsListFromRemote(onLoadKeywordsSuccess = { responseList ->
-
                     insertKeywordsDB(responseList) { result ->
                         Log.d(TAG, "Insert result: $result")
-
-                        getKeywordListFromDB(onLoadKeywordsSuccess = { resultList ->
-                            onLoadKeywordsSuccess(resultList)
-                        }, onLoadKeywordsError = onLoadKeywordsError)
+                        getKeywordsListFromDB(onLoadKeywordsSuccess = { resultList ->
+                            launch(UI) {
+                                onLoadKeywordsSuccess(resultList)
+                            }
+                        }, onLoadKeywordsError = {
+                            launch(UI) {
+                                onLoadKeywordsError()
+                            }
+                        })
                     }
-                }, onLoadKeywordsError = onLoadKeywordsError)
+                }, onLoadKeywordsError = {
+                    launch(UI) {
+                        onLoadKeywordsError()
+                    }
+                })
             } else {
                 Log.d(TAG, "Local is updated, get from local")
-
-                getKeywordListFromDB(onLoadKeywordsSuccess = { resultList ->
-                    onLoadKeywordsSuccess(resultList)
-                }, onLoadKeywordsError = onLoadKeywordsError)
+                getKeywordsListFromDB(onLoadKeywordsSuccess = { resultList ->
+                    launch(UI) {
+                        onLoadKeywordsSuccess(resultList)
+                    }
+                }, onLoadKeywordsError = {
+                    launch(UI) {
+                        onLoadKeywordsError()
+                    }
+                })
             }
         }
     }
 
     // Get from remote
+    @WorkerThread
     private fun fetchKeywordsListFromRemote(onLoadKeywordsSuccess: (List<Keyword>) -> Unit, onLoadKeywordsError: () -> Unit) {
-        mApiService.getKeywordsList().enqueue(object : Callback<List<String>> {
-            override fun onFailure(call: Call<List<String>>, t: Throwable) {
-                Log.d(TAG, "onFailure errorMessage=${t.message}")
-                launch(UI) {
+        launch {
+            mApiService.getKeywordsList().enqueue(object : Callback<List<String>> {
+                override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                    Log.d(TAG, "onFailure errorMessage=${t.message}")
                     onLoadKeywordsError()
                 }
-            }
 
-            override fun onResponse(call: Call<List<String>>, response: Response<List<String>>) {
-                Log.d(TAG, "onResponse code=${response.code()}, message=${response.message()}, body=${response.body()}")
-                response.body()?.let { responseList ->
-                    launch(UI) {
+                override fun onResponse(call: Call<List<String>>, response: Response<List<String>>) {
+                    Log.d(TAG, "onResponse code=${response.code()}, message=${response.message()}, body=${response.body()}")
+                    response.body()?.let { responseList ->
                         onLoadKeywordsSuccess(stringsToKeywordsList(responseList))
                     }
                 }
-            }
-        })
+            })
+        }
     }
 
     // Get from local
-    private fun getKeywordListFromDB(onLoadKeywordsSuccess: (List<Keyword>) -> Unit, onLoadKeywordsError: () -> Unit) {
+    @WorkerThread
+    private fun getKeywordsListFromDB(onLoadKeywordsSuccess: (List<Keyword>) -> Unit, onLoadKeywordsError: () -> Unit) {
         launch {
             try {
                 mKeywordsDAO.getKeywordsList().let { localList ->
                     Log.d(TAG, "Local list size=${localList.size}")
-                    launch(UI) {
-                        onLoadKeywordsSuccess(localList)
-                    }
+                    onLoadKeywordsSuccess(localList)
                 }
             } catch (exception: Exception) {
                 Log.d(TAG, "Get keywords from DB error=$exception")
@@ -94,6 +106,7 @@ object KeywordsRepository {
     }
 
     // Insert to local DB
+    @WorkerThread
     private fun insertKeywordsDB(list: List<Keyword>, onKeywordsInserted: (Boolean) -> Unit) {
         if (list.isEmpty()) {
             onKeywordsInserted(true)
@@ -101,14 +114,10 @@ object KeywordsRepository {
         launch {
             try {
                 mKeywordsDAO.insert(*list.toTypedArray())
-                launch(UI) {
-                    onKeywordsInserted(true)
-                }
+                onKeywordsInserted(true)
             } catch (exception: Exception) {
                 Log.d(TAG, "Insert keywords list error=$exception")
-                launch(UI) {
-                    onKeywordsInserted(false)
-                }
+                onKeywordsInserted(false)
             }
         }
     }
